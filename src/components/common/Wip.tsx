2ab6c51c10a9e4a0ddbe0dc1fb9e42148bbea00a
olhkyle <kylekwon.dev@gmail.com>
2025-12-18 17:31:12 +0900

    ui : add cn with props className

--- src/components/common/Wip.tsx
+++ src/components/common/Wip.tsx
@@ -1,14 +1,22 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
 export default function Wip({
   message,
   icon,
+  className = "",
 }: {
   message: string;
   icon: React.ReactNode;
+  className?: string;
 }) {
   return (
-    <div className="flex items-center gap-3 px-6 py-3 w-full text-white bg-gradient-orange-100 rounded-full">
+    <div
+      className={cn(
+        `flex items-center gap-3 px-6 py-3 w-full text-white bg-gradient-orange-100 rounded-full`,
+        className
+      )}
+    >
       <span>{icon}</span>
       <p className="font-medium text-xs sm:text-sm">{message}</p>
     </div>
