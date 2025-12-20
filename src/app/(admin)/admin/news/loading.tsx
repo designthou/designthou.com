f8dc1bdfbefd8721857f2fea784b83ce732728e3
olhkyle <itskkyle@gmail.com>
2025-12-20 16:29:56 +0900

    ui : change grid-cols of loading UI on AdminNewsPage

--- src/app/(admin)/admin/news/loading.tsx
+++ src/app/(admin)/admin/news/loading.tsx
@@ -4,7 +4,7 @@ export default function NewsPageLoading() {
   return (
     <div className="flex flex-col gap-2 p-4">
       <Skeleton className="h-8 w-[120px]" />
-      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
         {Array.from({ length: 8 }, (_, idx) => (
           <Skeleton key={idx} className="h-20 w-full" />
         ))}
