ec26fd0025cbbf744effbb6075bfb98bf23a8afe
olhkyle <kylekwon.dev@gmail.com>
2026-01-09 17:05:48 +0900

    feat : update config with static metaData

--- src/app/og/static/route.tsx
+++ src/app/og/static/route.tsx
@@ -3,52 +3,27 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const title = searchParams.get("title") ?? "Designthou";
+  const url = new URL(request.url);
+  const title = url.searchParams.get("title") || "Designthou";
 
   return new ImageResponse(
     <div
+      tw="flex flex-col w-full h-full items-center justify-center"
       style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
         background: `
-        radial-gradient(
-          circle at 20% 80%,
-          #fed7aa 0%,
-          #f87171 45%,
-          #fff7ed 100%
-        )
-      `,
+      radial-gradient(
+        circle at 20% 80%,
+        #fed7aa 0%,
+        #f87171 45%,
+        #fff7ed 100%
+      )
+    `,
       }}
     >
-      <div tw="flex flex-col items-center">
-        <h1
-          style={{
-            fontSize: 64,
-            fontWeight: 800,
-            color: "white", // orange-900 느낌
-            textAlign: "center",
-            padding: "0 80px",
-            lineHeight: 1.2,
-          }}
-        >
-          Designthou
-        </h1>
-        <p
-          style={{
-            fontSize: 32,
-            fontWeight: 600,
-            color: "white", // orange-900 느낌
-            textAlign: "center",
-            padding: "0 40px",
-            lineHeight: 1.2,
-          }}
-        >
+      <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+        <h1 tw="flex flex-col text-4xl font-black tracking-tight text-center">
           {title}
-        </p>
+        </h1>
         <p tw="mt-4 text-2xl text-gray-500">Architecture & Design Platform</p>
       </div>
     </div>,
--- src/app/og/tips/route.tsx
+++ src/app/og/tips/route.tsx
@@ -7,11 +7,24 @@ export async function GET(request: Request) {
   const title = url.searchParams.get("title") || "Designthou";
 
   return new ImageResponse(
-    <div tw="flex flex-col w-full h-full items-center justify-center bg-gradient-orange-100">
+    <div
+      tw="flex flex-col w-full h-full items-center justify-center"
+      style={{
+        background: `
+    radial-gradient(
+      circle at 20% 80%,
+      #fed7aa 0%,
+      #f87171 45%,
+      #fff7ed 100%
+    )
+  `,
+      }}
+    >
       <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
-        <h2 tw="flex flex-col text-4xl font-bold tracking-tight text-center">
+        <h1 tw="flex flex-col text-4xl font-black tracking-tight text-center">
           {title}
-        </h2>
+        </h1>
+        <p tw="mt-4 text-2xl text-gray-500">Architecture & Design Platform</p>
       </div>
     </div>,
     {
