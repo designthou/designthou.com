0ac99bd38ea5152c23feb4a7536912fd273807ff
olhkyle <itskkyle@gmail.com>
2026-01-12 19:25:32 +0900

    refactor : update schema restriction

--- src/components/(auth)/auth/LoginForm.tsx
+++ src/components/(auth)/auth/LoginForm.tsx
@@ -1,120 +1,120 @@
-"use client";
+'use client'
 
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useLogin } from "@/hooks";
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useLogin } from '@/hooks'
 import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Button,
-  Input,
-  loginSchema,
-  type LoginSchema,
-  AnimateLoader,
-} from "@/components";
-import { route } from "@/constants";
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+	Button,
+	Input,
+	loginSchema,
+	type LoginSchema,
+	AnimateLoader,
+} from '@/components'
+import { route } from '@/constants'
 
 export default function LoginForm() {
-  const form = useForm<LoginSchema>({
-    resolver: zodResolver(loginSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
+	const form = useForm<LoginSchema>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			email: '',
+			password: '',
+		},
+	})
 
-  const router = useRouter();
-  const { mutate: signup, isPending } = useLogin();
+	const router = useRouter()
+	const { mutate: signup, isPending } = useLogin()
 
-  const onSubmit = async (values: LoginSchema) => {
-    const { email, password } = values;
+	const onSubmit = async (values: LoginSchema) => {
+		const { email, password } = values
 
-    signup(
-      { email, password },
-      {
-        onSuccess: () => {
-          toast.success("로그인 성공");
-          router.push(route.ADMIN.ROOT);
-          router.refresh();
-        },
+		signup(
+			{ email, password },
+			{
+				onSuccess: () => {
+					toast.success('로그인 성공')
+					router.push(route.ADMIN.ROOT)
+					router.refresh()
+				},
 
-        onError: (error) => {
-          console.log(error);
-          toast.error(error?.message);
-        },
-      },
-    );
-  };
+				onError: error => {
+					console.error(error)
+					toast.error(error?.message)
+				},
+			},
+		)
+	}
 
-  return (
-    <div className="flex flex-col gap-4 mt-8 p-4 bg-white rounded-lg">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-4 mt-8"
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input
-                    type="email"
-                    placeholder="hello-designthou@gmail.com"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <div className="flex justify-between items-center">
-                  <FormLabel>Password</FormLabel>
-                  <Button
-                    asChild
-                    variant="link"
-                    size="sm"
-                    className="h-auto text-gray-500 text-center"
-                  >
-                    <Link href={route.AUTH.FORGOT_PASSWORD}>
-                      Forgot Password?
-                    </Link>
-                  </Button>
-                </div>
-                <FormControl>
-                  <Input type="password" placeholder="********" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" variant="default" size="lg">
-            {isPending ? <AnimateLoader /> : "Login"}
-          </Button>
-        </form>
-      </Form>
+	return (
+		<div className="flex flex-col gap-4 mt-8 p-4 bg-white rounded-lg">
+			<Form {...form}>
+				<form
+					onSubmit={form.handleSubmit(onSubmit)}
+					className="flex flex-col gap-4 mt-8"
+				>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										type="email"
+										placeholder="hello-designthou@gmail.com"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<div className="flex justify-between items-center">
+									<FormLabel>Password</FormLabel>
+									<Button
+										asChild
+										variant="link"
+										size="sm"
+										className="h-auto text-gray-500 text-center"
+									>
+										<Link href={route.AUTH.FORGOT_PASSWORD}>
+											Forgot Password?
+										</Link>
+									</Button>
+								</div>
+								<FormControl>
+									<Input type="password" placeholder="********" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<Button type="submit" variant="default" size="lg">
+						{isPending ? <AnimateLoader /> : 'Login'}
+					</Button>
+				</form>
+			</Form>
 
-      <Button
-        asChild
-        variant="link"
-        className="mx-auto text-center transition-all"
-      >
-        <Link href={route.AUTH.SIGNUP}>Do you need to register?</Link>
-      </Button>
-    </div>
-  );
+			<Button
+				asChild
+				variant="link"
+				className="mx-auto text-center transition-all"
+			>
+				<Link href={route.AUTH.SIGNUP}>Do you need to register?</Link>
+			</Button>
+		</div>
+	)
 }
--- src/components/(auth)/auth/schema.ts
+++ src/components/(auth)/auth/schema.ts
@@ -1,36 +1,36 @@
-import { z } from "zod";
+import { z } from 'zod'
 
-type LoginSchema = z.infer<typeof loginSchema>;
-type SignUpSchema = z.infer<typeof signUpSchema>;
+type LoginSchema = z.infer<typeof loginSchema>
+type SignUpSchema = z.infer<typeof signUpSchema>
 
 const loginSchema = z.object({
-  email: z.email({ message: "Email is invalid" }),
-  password: z
-    .string()
-    .min(1, { message: "Password cannot be blank" })
-    .regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,15}$/, {
-      message: `Write correct password format`,
-    }),
-});
+	email: z.email({ message: 'Email is invalid' }),
+	password: z
+		.string()
+		.min(1, { message: '비밀번호를 최소 7자 이상 입력해 주세요' })
+		.regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,18}$/, {
+			message: `올바른 비밀번호 형식을 입력해주세요 (숫자, 특수문자 포함)`,
+		}),
+})
 
 const signUpSchema = z
-  .object({
-    email: z.email({ message: "이메일 형식이 올바르지 않습니다." }),
-    password: z
-      .string()
-      .min(1, { message: "Password cannot be blank" })
-      .regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,15}$/, {
-        message: `올바른 비밀번호 형식을 입력해주세요 (특수문자 포함)`,
-      }),
-    confirmPassword: z.string().regex(/.+/, {
-      message: "확인을 위해 패스워드를 한 번 더 입력해 주세요",
-    }),
-    nickname: z.string().min(2, { message: "2글자 이상 작성해주세요" }),
-  })
-  .refine(({ confirmPassword, password }) => confirmPassword === password, {
-    path: ["confirmPassword"],
-    message: "패스워드가 일치하지 않습니다.",
-  });
+	.object({
+		email: z.email({ message: '이메일 형식이 올바르지 않습니다.' }),
+		password: z
+			.string()
+			.min(1, { message: '비밀번호를 최소 7자 이상 입력해 주세요' })
+			.regex(/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{7,18}$/, {
+				message: `올바른 비밀번호 형식을 입력해주세요 (숫자, 특수문자 포함)`,
+			}),
+		confirmPassword: z.string().regex(/.+/, {
+			message: '확인을 위해 패스워드를 한 번 더 입력해 주세요',
+		}),
+		nickname: z.string().min(2, { message: '2글자 이상 작성해 주세요' }),
+	})
+	.refine(({ confirmPassword, password }) => confirmPassword === password, {
+		path: ['confirmPassword'],
+		message: '패스워드가 일치하지 않습니다.',
+	})
 
-export type { LoginSchema, SignUpSchema };
-export { loginSchema, signUpSchema };
+export type { LoginSchema, SignUpSchema }
+export { loginSchema, signUpSchema }
