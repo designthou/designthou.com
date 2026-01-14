b8fe3e3677470a8c639ced20be52813df7f4d5e4
olhkyle <itskkyle@gmail.com>
2026-01-14 16:59:34 +0900

    ui : update ui of global-error on admin and service layout

--- src/app/(service)/global-error.tsx
+++ src/app/(service)/global-error.tsx
@@ -1,33 +1,31 @@
-"use client"; // Error components must be Client Components
+'use client'; // Error components must be Client Components
 
-import { Button } from "@/components";
-import { FileExclamationPoint, Rotate3d } from "lucide-react";
-import { useEffect } from "react";
+import Link from 'next/link';
+import React from 'react';
+import { FileExclamationPoint, Rotate3d } from 'lucide-react';
+import { Button } from '@/components';
+import { route } from '@/constants';
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+	React.useEffect(() => {
+		console.error(error);
+	}, [error]);
 
-  return (
-    <html>
-      <body>
-        <div className="ui-flex-center flex-1 w-screen h-screen">
-          <h2 className="font-bold text-xl">
-            <FileExclamationPoint size={18} /> 페이지에 문제가 있습니다.
-          </h2>
-          <Button onClick={() => reset()}>
-            <Rotate3d size={18} /> Try Again
-          </Button>
-          <Button type="button">Go to HOME</Button>
-        </div>
-      </body>
-    </html>
-  );
+	return (
+		<html>
+			<body>
+				<div className="ui-flex-center flex-1 w-screen h-screen">
+					<h2 className="font-bold text-xl">
+						<FileExclamationPoint size={18} /> 페이지에 문제가 있습니다.
+					</h2>
+					<Button onClick={() => reset()} variant="secondary">
+						<Rotate3d size={18} /> 재시도
+					</Button>
+					<Button type="button" asChild>
+						<Link href={route.SERVICE.ROOT}>홈으로 가기</Link>
+					</Button>
+				</div>
+			</body>
+		</html>
+	);
 }
