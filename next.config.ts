0d9fcd20c90b98f9a64f02bd2a087c8c03b3123f
olhkyle <kylekwon.dev@gmail.com>
2026-01-15 15:57:11 +0900

    feat : update env HOSTNAME

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME!,
-        pathname: "/storage/v1/object/public/**",
-      },
-    ],
-  },
+	/* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: process.env.NEXT_PUBLIC_SUPABASE_HOSTNAME!,
+				pathname: '/storage/v1/object/public/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
