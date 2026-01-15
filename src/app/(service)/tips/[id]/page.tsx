5d3384e8e5da24e780880a2a64c5c397beb1fa6b
olhkyle <kylekwon.dev@gmail.com>
2026-01-15 23:36:15 +0900

    ui : change ui of /tip/[id]

--- src/app/(service)/tips/[id]/page.tsx
+++ src/app/(service)/tips/[id]/page.tsx
@@ -1,94 +1,73 @@
-import Link from "next/link";
-import dynamic from "next/dynamic";
-import { ArrowLeft, ArrowUpRight, Download } from "lucide-react";
-import { AnimateLoader, Button } from "@/components";
-import { createClient } from "@/lib/supabase/server";
-import { convertSupabaseDateToShortHumanReadable } from "@/lib/supabase";
-import { route } from "@/constants";
+import Link from 'next/link';
+import dynamic from 'next/dynamic';
+import { ArrowLeft, ArrowUpRight, Download } from 'lucide-react';
+import { AnimateLoader, Button } from '@/components';
+import { createClient } from '@/lib/supabase/server';
+import { convertSupabaseDateToShortHumanReadable } from '@/lib/supabase';
+import { route } from '@/constants';
 
-const YoutubeVideo = dynamic(
-  () => import("@/components/(service)/tips/YoutubeVideo"),
-  {
-    loading: () => (
-      <div className="ui-flex-center min-h-50 bg-muted rounded-lg">
-        <AnimateLoader />
-      </div>
-    ),
-  }
-);
+const YoutubeVideo = dynamic(() => import('@/components/(service)/tips/YoutubeVideo'), {
+	loading: () => (
+		<div className="ui-flex-center min-h-50 bg-muted rounded-lg">
+			<AnimateLoader />
+		</div>
+	),
+});
 
-export default async function TipVideoPage({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  const supabase = await createClient();
-  const { data, error } = await supabase
-    .from("youtube_tips")
-    .select("*")
-    .eq("id", id)
-    .single();
+export default async function TipVideoPage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params;
+	const supabase = await createClient();
+	const { data, error } = await supabase.from('youtube_tips').select('*').eq('id', id).single();
 
-  if (error) {
-    throw new Error(error.message);
-  }
+	if (error) {
+		throw new Error(error.message);
+	}
 
-  return (
-    <section className="mx-auto p-4">
-      <Button variant="secondary" size="lg" asChild>
-        <Link href={route.SERVICE.TIPS}>
-          <ArrowLeft size={18} />
-          Tips Page
-        </Link>
-      </Button>
-      <div className="ui-flex-center-between mt-8">
-        <h2 className="page-title">{data?.title}</h2>
-        <div className="px-3 py-1.5 w-fit bg-gray-100 text-gray-700 rounded-lg">
-          {convertSupabaseDateToShortHumanReadable(data?.created_at)}
-        </div>
-      </div>
-      <div className="mt-8 rounded-lg">
-        <YoutubeVideo url={data?.url} />
-      </div>
-      <div className="mt-8 p-4 w-full bg-muted rounded-lg">
-        <Button type="button" asChild className="ml-auto">
-          {data.download_url && (
-            <Link href={data?.download_url} target="_blank" rel="noopener">
-              <Download size={18} />
-              Download
-            </Link>
-          )}
-        </Button>
-        <dl className="flex flex-col gap-4 px-3 py-1.5 w-full">
-          <dt className="flex items-center gap-4">
-            <span className="font-semibold">
-              디자인도우 건축 디자인 오픈채팅방
-            </span>
-            <Button type="button" variant="default" asChild>
-              <Link
-                href="https://open.kakao.com/o/gzL5v2Hd"
-                target="_blank"
-                rel="noopenner noreferrer"
-              >
-                바로가기
-                <ArrowUpRight size={18} />
-              </Link>
-            </Button>
-          </dt>
-          <dd className="flex items-center gap-2 w-full">
-            <p>
-              건축, 인테리어 작업에 도움이 필요할 때 언제든지 질문 가능합니다
-            </p>
-            <div className="flex items-center gap-2 px-3 py-1.5 bg-light border border-orange-200 rounded-lg">
-              <span>참여코드</span>-
-              <span className="inline-block px-3 py-1.5 font-black text-white bg-gradient-orange-100 rounded-md">
-                디도 (eleh)
-              </span>
-            </div>
-          </dd>
-        </dl>
-      </div>
-    </section>
-  );
+	return (
+		<section className="mx-auto p-4">
+			<Button variant="secondary" size="lg" asChild>
+				<Link href={route.SERVICE.TIPS}>
+					<ArrowLeft size={18} />
+					Tips Page
+				</Link>
+			</Button>
+			<div className="flex flex-col justify-between gap-2 mt-8 sm:items-center sm:flex-row">
+				<h2 className="page-title">{data?.title}</h2>
+				<div className="px-3 py-1.5 w-fit bg-gray-100 text-gray-700 rounded-lg">
+					{convertSupabaseDateToShortHumanReadable(data?.created_at)}
+				</div>
+			</div>
+			<div className="mt-8 rounded-lg">
+				<YoutubeVideo url={data?.url} />
+			</div>
+			<div className="mt-8 p-4 w-full bg-light rounded-lg">
+				<Button type="button" asChild className="ml-auto">
+					{data.download_url && (
+						<Link href={data?.download_url} target="_blank" rel="noopener">
+							<Download size={18} />
+							Download
+						</Link>
+					)}
+				</Button>
+				<dl className="flex flex-col gap-8 px-3 py-1.5 w-full">
+					<dt className="flex items-center gap-4">
+						<span className="font-semibold">디자인도우 건축 디자인 오픈채팅방</span>
+						<Button type="button" variant="default" asChild>
+							<Link href="https://open.kakao.com/o/gzL5v2Hd" target="_blank" rel="noopenner noreferrer">
+								바로가기
+								<ArrowUpRight size={18} />
+							</Link>
+						</Button>
+					</dt>
+					<dd className="flex flex-col gap-2 w-full sm:flex-row sm:items-center">
+						<p>건축, 인테리어 작업에 도움이 필요할 때 언제든지 질문 가능합니다</p>
+						<div className="flex items-center gap-2 px-3 py-1.5 bg-light border border-orange-200 rounded-lg">
+							<span>참여코드</span>-
+							<span className="inline-block px-3 py-1.5 font-black text-white bg-gradient-orange-100 rounded-md">디도 (eleh)</span>
+						</div>
+					</dd>
+				</dl>
+			</div>
+		</section>
+	);
 }
--- src/components/(service)/tips/TipList.tsx
+++ src/components/(service)/tips/TipList.tsx
@@ -1,74 +1,63 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import Image from "next/image";
-import { Sparkle } from "lucide-react";
-import { AnimateLoader, Wip } from "@/components";
-import { useInfiniteScroll, useTipList } from "@/hooks";
-import { convertSupabaseDateToShortHumanReadable } from "@/lib/supabase";
-import { generateGradient } from "@/utils/seedGradient";
+import Link from 'next/link';
+import Image from 'next/image';
+import { Sparkle } from 'lucide-react';
+import { AnimateLoader, Wip } from '@/components';
+import { useInfiniteScroll, useTipList } from '@/hooks';
+import { convertSupabaseDateToShortHumanReadable } from '@/lib/supabase';
+import { generateGradient } from '@/utils/seedGradient';
 
 export default function TipList() {
-  const { data, hasTipList, hasNextPage, fetchNextPage, isLoading } =
-    useTipList();
+	const { data, hasTipList, hasNextPage, fetchNextPage, isLoading } = useTipList();
 
-  const targetRef = useInfiniteScroll<HTMLDivElement>({
-    callback: fetchNextPage,
-    isLoading,
-    hasNextPage,
-  });
+	const targetRef = useInfiniteScroll<HTMLDivElement>({
+		callback: fetchNextPage,
+		isLoading,
+		hasNextPage,
+	});
 
-  return (
-    <>
-      <ul className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
-        {!hasTipList && (
-          <Wip icon={<Sparkle size={18} />} message={`Empty Data`} />
-        )}
-        {data?.map(({ id, title, imgSrc, url, category, created_at }) => (
-          <li
-            key={id}
-            className="flex items-center bg-light border border-muted rounded-lg"
-          >
-            <Link
-              href={`/tips/${id}`}
-              className="flex flex-col justify-between gap-4 p-3 w-full h-full rounded-lg"
-            >
-              {imgSrc ? (
-                <Image
-                  src={imgSrc}
-                  alt={title}
-                  width={500}
-                  height={300}
-                  placeholder="blur"
-                  blurDataURL="data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8Uw8AAh0BTZud3BwAAAAASUVORK5CYII="
-                  className="aspect-[5/2.8] rounded-lg object-cover"
-                />
-              ) : (
-                <div
-                  className="aspect-4/3 p-4 min-w-[50px] max-h-[50px] rounded-full sm:min-w-[100px] sm:max-h-none sm:rounded-lg"
-                  style={{ background: generateGradient(url) }}
-                />
-              )}
-              <div className="flex flex-col justify-between gap-4 w-full h-full">
-                <div className="text-base font-bold sm:text-lg">{title}</div>
-                <div className="ui-flex-center-between">
-                  <span className="text-gray-700 text-xs sm:text-sm">
-                    {convertSupabaseDateToShortHumanReadable(created_at)}
-                  </span>
-                  <span className="px-2 py-1 text-gray-700 text-xs font-semibold border border-full border-gray-300 rounded-lg sm:text-sm">
-                    {category}
-                  </span>
-                </div>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      {hasNextPage && hasTipList && (
-        <div ref={targetRef} className="ui-flex-center h-32">
-          <AnimateLoader />
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<ul className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+				{!hasTipList && <Wip icon={<Sparkle size={18} />} message={`Empty Data`} />}
+				{data?.map(({ id, title, imgSrc, url, category, created_at }) => (
+					<li key={id} className="flex items-center bg-light border border-muted rounded-lg">
+						<Link href={`/tips/${id}`} className="flex flex-col justify-between gap-4 p-3 w-full h-full rounded-lg">
+							{imgSrc ? (
+								<Image
+									src={imgSrc}
+									alt={title}
+									width={500}
+									height={300}
+									placeholder="blur"
+									blurDataURL="data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mM8Uw8AAh0BTZud3BwAAAAASUVORK5CYII="
+									className="aspect-[5/2.8] rounded-lg object-cover"
+								/>
+							) : (
+								<div
+									className="aspect-4/3 p-4 min-w-[50px] max-h-[50px] rounded-full sm:min-w-[100px] sm:max-h-none sm:rounded-lg"
+									style={{ background: generateGradient(url) }}
+								/>
+							)}
+							<div className="flex flex-col justify-between gap-4 w-full h-full">
+								<div className="text-base font-bold sm:text-lg">{title}</div>
+								<div className="ui-flex-center-between">
+									<span className="text-gray-700 text-xs sm:text-sm">{convertSupabaseDateToShortHumanReadable(created_at)}</span>
+									<span className="px-2 py-1 text-gray-700 text-xs font-semibold border border-full border-gray-300 rounded-lg sm:text-sm">
+										{category}
+									</span>
+								</div>
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+			{hasNextPage && hasTipList && (
+				<div ref={targetRef} className="ui-flex-center h-32">
+					<AnimateLoader />
+				</div>
+			)}
+		</>
+	);
 }
