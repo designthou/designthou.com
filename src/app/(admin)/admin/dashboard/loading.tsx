1d2f57632cb16887aec29cc106f5132e98317cd7
olhkyle <kylekwon.dev@gmail.com>
2026-01-17 19:19:45 +0900

    ui : remove pl-16

--- src/app/(admin)/admin/dashboard/loading.tsx
+++ src/app/(admin)/admin/dashboard/loading.tsx
@@ -1,5 +1,5 @@
 import { LayoutLoader } from '@/components';
 
 export default function DashboardLoading() {
-	return <LayoutLoader className="pl-14 lg:pl-56" />;
+	return <LayoutLoader className="lg:pl-56" />;
 }
--- src/app/(admin)/admin/loading.tsx
+++ src/app/(admin)/admin/loading.tsx
@@ -1,5 +1,5 @@
 import { LayoutLoader } from '@/components';
 
 export default function Loading() {
-	return <LayoutLoader className="pl-14 lg:pl-56" />;
+	return <LayoutLoader className="lg:pl-56" />;
 }
