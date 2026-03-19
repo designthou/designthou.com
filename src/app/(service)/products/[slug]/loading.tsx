5bfd17ca3aae8407dcfcf7efa899dd77c2120e38
kyle-air <kylekwon.dev@gmail.com>
2026-03-19 20:05:31 +0900

    ui : update loading.tsx on /products

--- src/app/(service)/products/[slug]/loading.tsx
+++ src/app/(service)/products/[slug]/loading.tsx
@@ -2,8 +2,15 @@ import { LayoutContainerLoader, Skeleton } from '@/components';
 
 export default function ProductsSlugLoading() {
 	return (
-		<section className="max-w-300">
+		<section className="flex flex-col gap-4 max-w-300">
+			<Skeleton className="w-25 h-9" />
 			<Skeleton className="w-full h-95" />
+			<Skeleton className="mt-8 w-30 h-9" />
+			<div className="grid grid-cols-1 gap-4 mt-8 md:grid-cols-3">
+				{Array.from({ length: 3 }, (_, idx) => (
+					<Skeleton key={idx} className="col-span-1 h-95" />
+				))}
+			</div>
 			<LayoutContainerLoader />;
 		</section>
 	);
